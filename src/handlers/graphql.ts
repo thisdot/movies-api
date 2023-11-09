b77d3d7c8344f18cb2784681907d58e7698e89bc
jesus padron <jesuspadron4497@gmail.com>
2023-11-09 17:03:40 -0400

    chore: delete redis from the project

--- src/handlers/graphql.ts
+++ src/handlers/graphql.ts
@@ -1,11 +1,9 @@
 import { ApolloServer, BaseContext } from '@apollo/server';
 import { startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
-import { KeyvAdapter } from '@apollo/utils.keyvadapter';
 import { resolvers, typeDefs } from '../schema';
-import { redisClient } from '../utils/redis';
 import TechnologyModel from '../models/Technology';
 
-const { REDIS_CACHE_TTL_SECONDS = 900 } = process.env;
+const { CACHE_TTL_SECONDS = 900 } = process.env;
 
 export interface MyContext extends BaseContext {
 	dataSources: {
@@ -15,9 +13,8 @@ export interface MyContext extends BaseContext {
 export const apolloServer = new ApolloServer<MyContext>({
 	typeDefs,
 	resolvers,
-	cache: new KeyvAdapter(redisClient),
 	persistedQueries: {
-		ttl: Number(REDIS_CACHE_TTL_SECONDS),
+		ttl: Number(CACHE_TTL_SECONDS),
 	},
 	formatError: (formattedError, error) => {
 		if (!(error instanceof Error)) return formattedError;
