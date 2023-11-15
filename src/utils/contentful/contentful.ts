50ef792ee44c9e45bce997da2e32885bc5d2fe4a
William Takashi Mimura <william.mimura@thisdot.co>
2023-11-15 12:41:24 -0800

    chore: refactor genre movies endpoint

--- src/utils/contentful/contentful.ts
+++ src/utils/contentful/contentful.ts
@@ -5,13 +5,23 @@ import * as dotenv from 'dotenv';
 // load dotenv config
 dotenv.config();
 
-export const CONTENTFUL_LIMIT = 25;
+export const DEFAULT_CONTENTFUL_LIMIT = 25;
 
+// Content Delivery API - https://www.contentful.com/developers/docs/references/content-delivery-api/
+// Use this to read data from Contentful
 export const cdaClient = createCDAClient({
 	space: `${process.env.CONTENTFUL_SPACE_ID}`,
 	accessToken: `${process.env.CONTENTFUL_DELIVERY_API_TOKEN}`,
 });
 
+// Content Management API - https://www.contentful.com/developers/docs/references/content-management-api/
+// Use this to manage content in your spaces
+//
+// From website:
+// Note: You can use the CMA to deliver and manage content, but you shouldn't use it to deliver large
+// amounts of content and instead use the Content Delivery API. The structure of responses from the
+// CMA differs from the CDA as GET responses retrieve the entirety of items (i.e. all localized and
+// unpublished content).
 const cmaClient = createClient({
 	accessToken: `${process.env.CONTENTFUL_CONTENT_MANAGEMENT_API_TOKEN}`,
 });
