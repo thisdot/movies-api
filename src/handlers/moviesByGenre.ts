6499763d3c62ebb3a5b92a6099f62d399cec3479
William Takashi Mimura <william.mimura@thisdot.co>
2023-11-16 15:22:36 -0800

    feat: add limit to api endpoint

--- src/handlers/moviesByGenre.ts
+++ src/handlers/moviesByGenre.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import getAll from '../models/Genre/getAll';
+import { DEFAULT_CONTENTFUL_LIMIT } from '../utils/contentful';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
 	let page = 1;
@@ -10,8 +11,16 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 		}
 	}
 
+	let limit = DEFAULT_CONTENTFUL_LIMIT;
+	if (event.queryStringParameters && event.queryStringParameters.limit) {
+		const parsedLimit = parseInt(event.queryStringParameters.limit, 10);
+		if (!isNaN(parsedLimit) && parsedLimit > 0) {
+			limit = parsedLimit;
+		}
+	}
+
 	try {
-		const result = await getAll({ page });
+		const result = await getAll({ page, limit });
 
 		return {
 			statusCode: 200,
