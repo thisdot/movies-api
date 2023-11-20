9190a4654de45c9337ec389dff3143c08564b2fb
jesus padron <jesuspadron4497@gmail.com>
2023-11-20 19:32:03 -0400

    fix: typo in comment

--- src/utils/api/utils.ts
+++ src/utils/api/utils.ts
@@ -1,6 +1,6 @@
-// Type guard function
 import { CustomContentfulError } from '../../types/contentful';
 
+// Type guard function to check if error is of type CustomContentfulError
 export function isCustomContentfulError(error: any): error is CustomContentfulError {
 	return error && typeof error === 'object' && 'sys' in error && 'id' in error.sys;
 }
