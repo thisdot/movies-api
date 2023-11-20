dbba7a178ce575eefa2f6ace17bf41caa2e3c457
William Takashi Mimura <william.mimura@thisdot.co>
2023-11-20 12:50:59 -0800

    fix: use correct include values

--- src/types/contentful.ts
+++ src/types/contentful.ts
@@ -1,7 +1,7 @@
 export const CONTENTFUL_INCLUDE = {
 	noInclude: 0, // this adds at least the linked IDs
-	movies: 2,
-	moviesWithGenre: 3,
+	movies: 1,
+	moviesWithGenre: 2,
 } as const;
 
 export type ContentfulIncludeOptions = (typeof CONTENTFUL_INCLUDE)[keyof typeof CONTENTFUL_INCLUDE];
