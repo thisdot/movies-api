22f8a21c56b738487f75c4d47fe996e9020dde8b
William Takashi Mimura <william.mimura@thisdot.co>
2023-11-21 13:47:42 -0800

    feat: add simple auth with fetch valid token
    Resolves #14

---
+++ src/handlers/auth/authorizer.ts
@@ -0,0 +1,24 @@
+import { APIGatewayRequestAuthorizerHandler } from 'aws-lambda';
+import { isTokenValid } from '../../utils/api/apiAuth';
+
+export const handler: APIGatewayRequestAuthorizerHandler = async (event) => {
+	const authorization = event.headers?.['Authorization'] || '';
+
+	// removing the initial "Bearer "
+	const token = authorization.substring(7);
+	const isAuthorized = isTokenValid(token);
+
+	return {
+		principalId: 'simpleAuthorizerPrincipal', // principal user identification associated with the token sent by the client - let's hardcode for simplicity
+		policyDocument: {
+			Version: '2012-10-17',
+			Statement: [
+				{
+					Effect: isAuthorized ? 'Allow' : 'Deny',
+					Action: ['execute-api:Invoke'],
+					Resource: ['*'], // all resources in this project - for this purpose this is fine
+				},
+			],
+		},
+	};
+};
---
+++ src/utils/api/apiAuth.ts
@@ -0,0 +1,20 @@
+// Genereated from https://jwt.io/
+export const authorizedJWTs = [
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvcGVuSnd0MCIsIm5hbWUiOiJPcGVuSldUWzBdIn0.49JQF4ICJeqxpiIZ9x748VVOHj6FElyRm1tNpFGqaUY',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvcGVuSnd0MSIsIm5hbWUiOiJPcGVuSldUWzFdIn0.n8x8GHYe8RQYKkAoMVMlw9-FMZ57bs0HrwxBeJn3hQM',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvcGVuSnd0MiIsIm5hbWUiOiJPcGVuSldUWzJdIn0.ZV0H5kJrzOsfk3guI9pHt1bp0xzuCEFiuS4bP1XbEZE',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvcGVuSnd0MyIsIm5hbWUiOiJPcGVuSldUWzNdIn0.J43mgsvQlOK0zn7APkizIRkKphNIeXwtcabyKxoUnZo',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvcGVuSnd0NCIsIm5hbWUiOiJPcGVuSldUWzRdIn0.FEyU1vuRnD1wDw2kxuks59N9paTibhKHT8U9cpFw1dM',
+];
+
+export function isTokenValid(token: string) {
+	if (!token) {
+		return false;
+	}
+	return authorizedJWTs.includes(token);
+}
+
+export function getRandomValidToken(): string {
+	const randomIdx = Math.floor(Math.random() * authorizedJWTs.length);
+	return authorizedJWTs[randomIdx];
+}
