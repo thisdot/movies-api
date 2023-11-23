0a012391d8b81de5335c95ea6e76577a3773a928
William Takashi Mimura <william.mimura@thisdot.co>
2023-11-23 09:43:39 -0800

    feat: add simple auth with fetch valid token (#27)
    * feat: add simple auth with fetch valid token
    
    Resolves #14
    
    * feat: add authorization to graphql
    
    * chore: add useful links for ref

---
+++ src/handlers/auth/authorizer.ts
@@ -0,0 +1,25 @@
+import { APIGatewayRequestAuthorizerHandler } from 'aws-lambda';
+import { isTokenValid } from '../../utils/api/apiAuth';
+
+export const handler: APIGatewayRequestAuthorizerHandler = async (event) => {
+	const authorization = event.headers?.['Authorization'] || '';
+
+	// removing the initial "Bearer "
+	const token = authorization.substring(7);
+	const isAuthorized = isTokenValid(token);
+
+	// ref: https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-lambda-authorizer.html#http-api-lambda-authorizer.payload-format-response
+	return {
+		principalId: 'simpleAuthorizerPrincipal', // principal user identification associated with the token sent by the client - let's hardcode for simplicity
+		policyDocument: {
+			Version: '2012-10-17',
+			Statement: [
+				{
+					Effect: isAuthorized ? 'Allow' : 'Deny',
+					Action: ['execute-api:Invoke'],
+					Resource: ['*'], // all resources in this project - for this purpose this is fine
+				},
+			],
+		},
+	};
+};
---
+++ src/handlers/auth/getValidToken.ts
@@ -0,0 +1,10 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { getRandomValidToken } from '../../utils/api/apiAuth';
+
+export const handler: APIGatewayProxyHandler = async () => {
+	const validToken = getRandomValidToken();
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ token: validToken }),
+	};
+};
